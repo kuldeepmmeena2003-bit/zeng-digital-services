3e8fc5d9ec647215c90c3f46f6414d5325344370
Kuldeep Meena <kuldeepmmeena2003@gmail.com>
2026-02-24 05:58:14 -0800

    Premium header footer final

---
+++ public/js/header.js
@@ -0,0 +1,8 @@
+window.addEventListener("scroll", function(){
+  const header = document.querySelector(".site-header");
+  if(window.scrollY > 20){
+    header.classList.add("scrolled");
+  }else{
+    header.classList.remove("scrolled");
+  }
+});
